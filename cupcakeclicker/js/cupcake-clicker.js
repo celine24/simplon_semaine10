fffa74d79a65300cd7aad6eb0a2be20efffe8279
celine24 <peyrot.celine@gmail.com>
2016-02-13 18:33:24 +0000

    cookie clicker etape 10

--- cupcakeclicker/js/cupcake-clicker.js
+++ cupcakeclicker/js/cupcake-clicker.js
@@ -1,24 +1,37 @@
 var button = document.getElementById('button');
 var div = document.getElementById('affichage');
-var multiplication = document.getElementById('multiplicateur');
+var multiplicateur = document.getElementById('multiplicateur');
+var autoclicker = document.getElementById('autoclick');
 var score = 0;
 var compteur = 1;
+var prixMultiplicateur = 10;
+var prixAutoclicker = 100;
 
-    button.addEventListener('click', function() {
-        score += compteur;
-        div.innerHTML = score;
-
-    }, false);
+function clic () {
+    score += compteur;
+    div.innerHTML = score;
+}
     
-    multiplication.addEventListener('click', function() {
-        if (score >= 10) {
-            compteur += 1;
-            score -= 10;
-            div.innerHTML = score + ' - vous avez acheté un multiplicateur !';
-            multiplication.innerHTML = 'Multiplicateur x' + compteur;
-        }
-        else {
-            div.innerHTML = score + ' - désolé, vous n\'avez pas assez d\'argent pour acheter un multiplicateur.';
-        }   
-    }, false);
+function multiplication() {
+    if (score >= prixMultiplicateur) {
+        compteur += 1;
+        score -= prixMultiplicateur;
+        prix += 20;
+        div.innerHTML = score + ' - vous avez acheté un multiplicateur !';
+        multiplicateur.innerHTML = 'Multiplicateur x' + compteur + ' - prix : ' + prixMultiplicateur;
+    }
+    else {
+        div.innerHTML = score + ' - désolé, vous n\'avez pas assez d\'argent pour acheter un multiplicateur.';
+    }   
+}
 
+function autoclick() {
+    
+
+}
+    
+button.onclick = clic;
+multiplicateur.onclick = multiplication;
+autoclicker.onclick = autoclick;
+
+setInterval("instructions", 1000);
