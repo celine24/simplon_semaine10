b7b8e511d00f824f2d995a71b73256844e60ebfc
celine24 <peyrot.celine@gmail.com>
2016-02-17 17:39:03 +0000

    cookie clicker fini

--- cupcakeclicker/js/cupcake-clicker.js
+++ cupcakeclicker/js/cupcake-clicker.js
@@ -1,44 +1,101 @@
-var button = document.getElementById('button');
-var div = document.getElementById('affichage');
-var multiplicateur = document.getElementById('multiplicateur');
-var autoclicker = document.getElementById('autoclick');
-var score = 0;
-var compteur = 1;
-var prixMultiplicateur = 50;
-var prixAutoclicker = 100;
+var cupcake = document.getElementById('cupcake');
+var egg = document.getElementById('egg');
+var mold = document.getElementById('mold');
+var mixer = document.getElementById('mixer');
 
-function clic () {
-    score += compteur;
-    div.innerHTML = score;
-}
-    
-function multiplication() {
-    if (score >= prixMultiplicateur) {
-        compteur += 1;
-        score -= prixMultiplicateur;
-        prixMultiplicateur += 50;
-        div.innerHTML = score + ' - vous avez acheté un multiplicateur !';
-        multiplicateur.innerHTML = 'Multiplicateur x' + compteur + ' - prix : ' + prixMultiplicateur;
+var nbCupcake = document.getElementById('nbCupcake');
+var nbEgg = document.getElementById('nbEgg');
+var nbMold = document.getElementById('nbMold');
+var nbMixer = document.getElementById('nbMixer');
+
+var priceEgg = document.getElementById('priceEgg');
+var priceMold = document.getElementById('priceMold');
+var priceMixer = document.getElementById('priceMixer');
+
+var ad = document.getElementById('ad');
+var score = 0;
+var nbEggs = 1;
+var nbMolds = 1;
+var nbMixers = 1;
+var compteur = 1;
+var costEgg = 10;
+var costMold = 50;
+var costMixer = 100;
+var plural;
+
+
+function convertPlural(nb) {
+    if (nb > 1) {
+        return 's';
     }
     else {
-        div.innerHTML = score + ' - désolé, vous n\'avez pas assez d\'argent pour acheter un multiplicateur.';
+        return '';
+    }
+}
+
+
+/*var mixer = 'mixer';*/
+
+function cook () {
+    score += compteur;
+    plural = convertPlural(score);
+    nbCupcake.innerHTML = score + ' cupcake' + plural;
+}
+
+
+function buyEgg() {
+    if (score >= costEgg) {
+        compteur += 1;
+        score -= costEgg;
+        costEgg *= 2;
+        nbEggs += 1;
+        plural = convertPlural(nbEggs);
+        nbCupcake.innerHTML = score + ' cupcake' + plural;
+        nbEgg.innerHTML = nbEggs + ' egg' + plural;
+        priceEgg.innerHTML = 'cost ' + costEgg;
+        ad.innerHTML = 'Congrats ! You bought another egg for ' + costEgg / 2 + '.';
+    }
+    else {
+        ad.innerHTML = 'Not enough cupcakes for egg.';
+    }
+}
+    
+function buyMold() {
+    if (score >= costMold) {
+        compteur += 5;
+        score -= costMold;
+        costMold *= 2;
+        nbMolds += 1;
+        plural = convertPlural(nbMolds);
+        nbCupcake.innerHTML = score + ' cupcake' + plural;
+        ad.innerHTML = 'Congrats ! You bought another cake mold for ' + costMold / 2 + '.';
+        nbMold.innerHTML = nbMolds + ' mold' + plural;
+        priceMold.innerHTML = 'cost ' + costMold;
+    }
+    else {
+        ad.innerHTML = 'Not enough cupcakes for mold.';
     }   
 }
 
-function autoclick() {
-    if (score >= prixAutoclicker) {
-        setInterval(clic, 1000);
-        prixAutoclicker += 100;
-        score -= prixAutoclicker;
-        div.innerHTML = score + ' - vous avez acheté un autoclicker !';
-        autoclicker.innerHTML = 'Autoclicker - prix : ' + prixAutoclicker;
+function buyMixer() {
+    if (score >= costMixer) {
+        setInterval(cook, 1000);
+        score -= costMixer;
+        costMixer *= 10;
+        nbMixers += 1;
+        plural = convertPlural(nbMixers);
+        nbCupcake.innerHTML = score + ' cupcake' + plural;
+        nbMixer.innerHTML = nbMixers + ' electric mixer' + plural;
+        priceMixer.innerHTML = 'cost ' + costMixer;
+        ad.innerHTML = 'Congrats ! You bought another electric mixer for ' + costMixer / 10 + '.';
     }
     else {
-        div.innerHTML = score + ' - désolé, vous n\'avez pas assez d\'argent pour acheter l\'autoclicker.';
+        ad.innerHTML = 'Not enough cupcakes for electric mixer.';
     }
 }
     
-button.onclick = clic;
-multiplicateur.onclick = multiplication;
-autoclicker.onclick = autoclick;
+cupcake.onclick = cook;
+egg.onclick = buyEgg;
+mold.onclick = buyMold;
+mixer.onclick = buyMixer;
 
